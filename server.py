3e8605e035d99beba99dae84fa45005217b29b45
Vlad <vladislavs.zadvornevs@gmail.com>
2021-05-25 15:30:52 +0300

    Flask app ready

---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+import os
+import random
+from keyword_spotting_service import KeywordSpottingService
+
+
+app = Flask(__name__)
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    # get audio file and save it
+    audio_file = request.files["file"]
+    file_name = str(random.randint(0, 100000))
+    audio_file.save(file_name)
+
+    # invoke keyword spotting service
+    kss = KeywordSpottingService()
+
+    # make prediction
+    predicted_keyword = kss.predict(file_name)
+
+    # remove the audio file
+    os.remove(file_name)
+
+    # send back the predicted keyword as json
+    data = {"keyword": predicted_keyword}
+
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
